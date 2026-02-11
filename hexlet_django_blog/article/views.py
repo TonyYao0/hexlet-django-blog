d64498b4671254e356088bb096c11fc1cd1ecf41
TonyYao0 <tony_yao@mail.ru>
2026-02-11 15:32:53 +0800

    Шаблонизация

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("article")
+    context ={
+        'app_name': 'Article'
+    }
+    return render(request, 'articles/index.html', context)
